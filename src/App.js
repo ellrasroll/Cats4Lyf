2fa629c909d2a9c2ffe386f0f987e866ba94f14f
ellrasroll <ellrasroll@gmail.com>
2023-04-26 15:23:40 +0100

    Added Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar>
+    
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+
+      </NavBar>
+
+  
+
+
     </div>
   );
 }
 
 export default App;
+
+const NavBar = styled.nav`
+  width: 100vw;
+  background-color: aliceblue;
+  box-sizing: border-box;
+  color: aliceblue;
+  padding: 20px;
+  display:flex;
+  justify-content: space-around;
+`
+
